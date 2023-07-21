450a572aac53135c0e9433e62ca27237139838a8
Matthieu SOUMBO <matthieusoumbo@orange.Fr>
2023-07-22 01:20:02 +0200

    First commit with Flask application

---
+++ overflow/app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request
+import preprocessing_code 
+import nltk
+import pickle
+# General Libraries
+import gensim
+import os
+import re
+import string
+from datetime import timedelta
+import datetime as dt
+
+# Data Manipulation Libraries
+import numpy as np
+import pandas as pd
+
+# Visualization Libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.patches import Circle
+from bs4 import BeautifulSoup
+
+# Machine Learning Libraries
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, MultiLabelBinarizer, QuantileTransformer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import SimpleImputer, IterativeImputer, KNNImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, silhouette_score, accuracy_score, make_scorer
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, Lasso, ElasticNet, SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, GradientBoostingRegressor
+from sklearn.dummy import DummyRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.multiclass import OneVsRestClassifier
+from scipy.stats import f_oneway, f
+from scipy import stats
+from scipy.sparse import csr_matrix
+from scipy.cluster.hierarchy import dendrogram, linkage, ward, fcluster
+import scipy.cluster.hierarchy as shc
+import scipy.stats as st
+
+
+
+# Natural Language Processing Libraries
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet, stopwords, names
+from nltk import pos_tag, word_tokenize
+
+
+# Topic Modelling Libraries
+
+from gensim.models import CoherenceModel
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+import gensim
+from sklearn.decomposition import LatentDirichletAllocation
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
+from sklearn.decomposition import LatentDirichletAllocation
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
+
+# Data Persistency
+import pickle
+
+# Download necessary NLTK data
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+# Chargement du modèle de prétraitement
+with open('modelpreprod.pkl', 'rb') as f:
+    preprocess_text = pickle.load(f)
+
+# Appel de la fonction init() pour charger le modèle LDA et le dictionnaire
+preprocessing_code.init()
+
+# Création d'une instance de l'application Flask
+app = Flask(__name__)
+
+# Initialiser le module de prétraitement au démarrage de l'application
+preprocessing_code.init()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        question = request.form['question']
+        tags = preprocessing_code.predict_tags(question)
+        return render_template('results.html', question=question, tags=tags)
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ overflow/preprocessing_code.py
@@ -0,0 +1,116 @@
+import re
+import string
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+from bs4 import BeautifulSoup
+import nltk
+import pickle
+import pandas as pd
+
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+# Initialisation globale du lemmatiseur, du modèle LDA et du dictionnaire
+lemmatizer = None
+lda_model = None
+dictionary = None
+
+def init():
+    global lemmatizer, lda_model, dictionary
+
+    # Télécharger les ressources nécessaires
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
+    nltk.download('stopwords')
+
+    # Initialiser le lemmatiseur de NLTK
+    lemmatizer = WordNetLemmatizer()
+
+    # Charger le modèle LDA
+    with open('C:/Users/matth/Documents/stack/lda_model.pkl', 'rb') as f:
+        lda_model = pickle.load(f)
+
+
+    # Charger le dictionnaire
+    with open('C:/Users/matth/Documents/stack/dictionary.pkl', 'rb') as f:
+        dictionary = pickle.load(f)
+
+# Initialiser le script
+init()
+
+
+# Fonction pour le prétraitement du texte
+def preprocess_text(text):
+    # Convertir le texte en minuscules
+    text = text.lower()
+
+    # Supprimer les URL
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+
+    # Supprimer les balises HTML
+    text = BeautifulSoup(text, "lxml").get_text()
+    
+    # Ajouter des espaces entre les mots collés
+    text = re.sub(r'([a-z])([A-Z])', r'\1 \2', text)
+    
+    # Supprimer la ponctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Lemmatisation avec NLTK et suppression des stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = word_tokenize(text)
+    text = " ".join([lemmatizer.lemmatize(token) for token in tokens if token not in stop_words])
+    
+    return text
+
+# Supposez que df est votre dataframe initial
+df = pd.read_csv("C:/Users/matth/python/QueryResults.csv")
+
+# Réduisez la taille de votre dataframe à 5000 lignes
+df = df.sample(n=5000, random_state=42)
+
+# Appliquer le prétraitement sur les colonnes 'Body' et 'Title'
+df['Body'] = df['Body'].apply(preprocess_text)
+df['Title'] = df['Title'].apply(preprocess_text)
+
+# Combinaison des colonnes 'Title' et 'Body' en une seule colonne 'Text'
+df['Text'] = df['Title'] + ' ' + df['Body']
+
+# Prétraiter les tags
+df['Tags'] = df['Tags'].str.replace('<', '').str.replace('>', ' ').str.split()
+
+# Sauvegarder le modèle de prétraitement
+with open('modelpreprod.pkl', 'wb') as f:
+    pickle.dump(preprocess_text, f)
+
+
+# Fonction pour prédire les tags
+def predict_tags(preprocessed_text):
+    # Convertir le texte prétraité en une représentation bag-of-words
+    bow = dictionary.doc2bow(preprocessed_text.split())
+    
+    # Utiliser le modèle LDA pour obtenir les topics les plus probables
+    topics = lda_model[bow]
+    
+    # Trier les topics par leur probabilité, et prendre les N topics les plus probables
+    sorted_topics = sorted(topics, key=lambda x: x[1], reverse=True)
+    top_topics = sorted_topics[:25]  # N est ici 5, mais vous pouvez choisir un autre nombre si vous le souhaitez
+    
+    # Pour chaque topic, obtenir le mot le plus probable et le traiter comme un "tag"
+    tags = []
+    for topic in top_topics:
+        word, _ = lda_model.show_topic(topic[0], topn=1)[0]  # Obtenir le mot le plus probable pour ce sujet
+        tags.append(word)
+    
+    return tags
+
+
+# Initialiser le script
+init()
+
+
